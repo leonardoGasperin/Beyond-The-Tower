8a4e33e23787f47d1f2c1aebe63070d0b6f7e6d4
Zaluth <regiswss@hotmail.com>
2025-08-05 20:17:08 -0300

    Movimentação

---
+++ Assets/2.Aplicacao/Servico/Movimentacao.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class Movimentacao : MonoBehaviour {
+
+    Rigidbody2D rb;
+    [SerializeField] private float velocidade = 10f;
+    [SerializeField] private float forcaDoPulo = 200f;
+
+    void Start(){
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update(){
+        rb.velocity = new Vector2(movimentoX * velocidade, 0f);
+    }
+
+    void Pulo(){
+        rb.AddForce(Vector2.up * forcaDoPulo, ForceMode2D.Impulse);
+    }
+}
